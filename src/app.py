18174f302492cddfa096c4c57e6bbbaadf8b167c
Renato Teixeira <rena2damas@gmail.com>
2021-12-27 16:21:47 -0500

    Add command for test

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 
+from src.cli.test import test_command
 from src.settings.config import config_by_name
 
 
@@ -15,4 +16,12 @@ def create_app(config_name='default'):
     env = os.getenv('FLASK_ENV', config_name)
     app.config.from_object(config_by_name[env])
 
+    setup_app(app)
+
     return app
+
+
+def setup_app(app):
+
+    # register cli commands
+    app.cli.add_command(test_command)
