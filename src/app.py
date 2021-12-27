55d074d5a9fb38daa6ed4116eef5188c81b8ed7d
Renato Teixeira <rena2damas@gmail.com>
2021-12-27 13:14:49 -0500

    chore: add flask default settings

---
+++ src/app.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask
+
+from src.settings.config import config_by_name
+
+
+def create_app(config_name=None):
+    """Create a new app."""
+
+    # define the WSGI application object
+    app = Flask(__name__)
+
+    # load object-based default configuration
+    env = os.getenv('FLASK_ENV', config_name)
+    app.config.from_object(config_by_name[env])
+
+    return app
