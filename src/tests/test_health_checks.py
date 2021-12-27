219ce515501d12f88714edf1974310b91835e45b
Renato Teixeira <rena2damas@gmail.com>
2021-12-27 13:37:03 -0500

    Add base tests

---
+++ src/tests/test_health_checks.py
@@ -0,0 +1,13 @@
+def test_get_health_check_by_id():
+    """GET request to retrieve an health check by id."""
+    pass
+
+
+def test_get_missing_health_check():
+    """GET request that cannot retrieve an health check by id."""
+    pass
+
+
+def test_get_health_checks():
+    """GET request to retrieve a list of health checks."""
+    pass
