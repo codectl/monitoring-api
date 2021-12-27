02ebf1723ae1789ed2a89249dbe001434ed79d1b
Renato Teixeira <rena2damas@gmail.com>
2021-12-27 13:40:36 -0500

    Add base tests

---
+++ src/tests/test_health_check_api.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestHealthCheckAPI(unittest.TestCase):
+
+    def test_get_health_check_by_id(self):
+        """GET request to retrieve an health check by id."""
+        pass
+
+    def test_get_missing_health_check(self):
+        """GET request that cannot retrieve an health check by id."""
+        pass
+
+    def test_get_health_checks(self):
+        """GET request to retrieve a list of health checks."""
+        pass
