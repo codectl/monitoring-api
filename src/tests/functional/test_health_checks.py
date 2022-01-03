837124772766865d9660e57b6e246343ed0520fd
Renato Teixeira <rena2damas@gmail.com>
2022-01-03 11:53:23 -0600

    test: add test for health checks #wip

--- src/tests/functional/test_health_checks.py
+++ src/tests/functional/test_health_checks.py
@@ -6,9 +6,9 @@ from src.app import create_app
 class TestHealthCheckAPI(unittest.TestCase):
 
     def setUp(self):
-        # create app for testing
         self.app = create_app(config_name='testing')
         self.client = self.app.test_client()
+        self.ctx = self.app.config['APPLICATION_CONTEXT']
 
         # self.bright = Bright(version='latest')
         # bright.health_checks()
@@ -21,6 +21,13 @@ class TestHealthCheckAPI(unittest.TestCase):
     #     response = self.client.get('/health-checks')
     #
     #     self.assertEqual(response.status_code, 200)
+
+    def test_get_health_checks(self):
+        """GET request to retrieve health checks."""
+        response = self.client.get(f"{self.ctx}/health-checks")
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(type(response.data), list)
 
     def test_get_health_check_by_id(self):
         """GET request to retrieve an health check by id."""
