f0253ca01518ea6da8b572faba0985fc08370ef4
Renato Teixeira <rena2damas@gmail.com>
2022-01-14 12:24:59 -0600

    refactor: add Bright 7 and 8 implementations

--- src/api/bright.py
+++ src/api/bright.py
@@ -1,7 +1,11 @@
+import abc
+import os
+
 import requests
+from flask import current_app
 
 
-class BrightBase:
+class BrightBase(abc.ABC):
     """Base class for Bright API."""
 
     default_headers = {
@@ -11,7 +15,7 @@ class BrightBase:
 
     def __init__(
             self,
-            host=None,
+            url=None,
             session=None,
             basic_auth=None,
             cert_auth=None,
@@ -25,7 +29,7 @@ class BrightBase:
         :param cert_auth: a tuple of cert and key to use when establishing a session. The pair is used for both
         authentication and encryption.
         """
-        self.host = host
+        self.url = url
         if session is None:
             self._session = requests.Session()
         else:
@@ -45,7 +49,7 @@ class BrightBase:
         self._session.cert = cert_auth
 
     def version(self):
-        url = "{0}/{1}".format(self.host, 'json')
+        url = "{0}/{1}".format(self.url, 'json')
         params = {
             'service': 'cmmain',
             'call': 'getVersion',
@@ -57,8 +61,19 @@ class BrightBase:
         ).json()
         return response.get('cmVersion')
 
+    @abc.abstractmethod
+    def measurable(self, name):
+        pass
+
+
+class Bright7(BrightBase):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.url = "{0}/{1}".format(self.url, 'json')
+
     def entity(self, name):
-        url = "{0}/{1}".format(self.host, 'json')
+        url = "{0}/{1}".format(self.url, 'json')
         params = {
             'service': 'cmdevice',
             'call': 'getDevice',
@@ -71,7 +86,7 @@ class BrightBase:
         ).json() or {}
 
     def measurable(self, name):
-        url = "{0}/{1}".format(self.host, 'json')
+        url = "{0}/{1}".format(self.url, 'json')
         params = {
             'service': 'cmmon',
             'call': 'getHealthcheck',
@@ -82,3 +97,63 @@ class BrightBase:
             json=params,
             verify=self.verify
         ).json() or {}
+
+
+class Bright8(BrightBase):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.url = "{0}/{1}/{2}".format(self.url, 'rest', 'v1')
+
+    def measurable(self, name):
+        url = "{0}/{1}".format(self.url, 'json')
+        params = {
+            'service': 'cmmon',
+            'call': 'getMonitoringMeasurable',
+            'arg': name
+        }
+        return self._session.post(
+            url,
+            json=params,
+            verify=self.verify
+        ).json() or {}
+
+
+class BrightAPI:
+
+    def __init__(
+            self,
+            host,
+            port=None,
+            cert_auth=None,
+            verify=False,
+            version=None
+    ):
+        host = host or current_app.config['BRIGHT_COMPUTING_HOST']
+        port = port or current_app.config['BRIGHT_COMPUTING_PORT']
+        url = f"https://{host}:{port}"
+
+        if not cert_auth:
+            cert = current_app.config['BRIGHT_COMPUTING_CERT_PATH']
+            key = current_app.config['BRIGHT_COMPUTING_KEY_PATH']
+            cert_auth = (cert, key)
+
+        version = version or BrightBase(url=url).version()
+        self.instance = self.factory(version)(
+            url=url,
+            cert_auth=cert_auth,
+            verify=verify
+        )
+
+    @staticmethod
+    def factory(version):
+        if isinstance(version, str):
+            version = int(float(version))
+        elif isinstance(version, float):
+            version = int(version)
+        if version == 7:
+            return Bright7
+        elif version == 8:
+            return Bright8
+        else:
+            raise ValueError('Unsupported version')
