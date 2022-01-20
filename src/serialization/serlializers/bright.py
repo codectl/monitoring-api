ae3fed3e0430464387aa2eea21bd4bb0a8a7828e
Renato Teixeira <rena2damas@gmail.com>
2022-01-20 11:21:19 -0600

    Add schema fields

--- src/serialization/serlializers/bright.py
+++ src/serialization/serlializers/bright.py
@@ -2,4 +2,9 @@ from marshmallow import Schema, fields
 
 
 class HealthCheckSchema(Schema):
+    name = fields.String()
     status = fields.String()
+    node = fields.String()
+    seconds_agp = fields.Integer()
+    timestamp = fields.Integer()
+    raw = fields.Raw()
