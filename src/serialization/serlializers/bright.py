4b79146f034f8ed77e2c6879643849dfbff912b7
Renato Teixeira <rena2damas@gmail.com>
2022-01-20 12:02:22 -0600

    Update status field

--- src/serialization/serlializers/bright.py
+++ src/serialization/serlializers/bright.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, fields
 
 class HealthCheckSchema(Schema):
     name = fields.String()
-    status = fields.String()
+    status = fields.String(attribute='status.name')
     node = fields.String()
     seconds_ago = fields.Integer()
     timestamp = fields.Integer()
