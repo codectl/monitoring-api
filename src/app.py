03acecce454677697dd42a249459b770f632dc7d
Renato Teixeira <rena2damas@gmail.com>
2022-01-31 21:46:44 +0000

    refactor: update imports #wip

--- src/app.py
+++ src/app.py
@@ -3,25 +3,19 @@ import os
 import flasgger
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
-from apispec_webframeworks.flask import FlaskPlugin
-from webframeworks.
+from apispec_plugins.webframeworks.flask import FlaskPlugin
 from flask import Blueprint, Flask, redirect, url_for
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_apispec import FlaskApiSpec, doc
 
 from src.cli.test import test_command
 from src.settings.config import config_by_name
-from src.schemas.serlializers.http import HttpErrorSchema
-from src.schemas.serlializers.bright import HealthCheckSchema
 
 # SQLite database
 db = SQLAlchemy()
 
 # initialize Flask Restful
 api = Api()
-
-docs = FlaskApiSpec(document_options=False)
 
 
 def create_app(config_name='default'):
@@ -50,9 +44,6 @@ def setup_app(app):
     # register api blueprint
     app.register_blueprint(api_bp)
 
-    # redirect root path to context root
-    # app.add_url_rule('/', 'index', lambda: redirect(url_for('flasgger.apidocs')))
-
     spec = APISpec(
         title=app.config['OPENAPI_SPEC']['info']['title'],
         version=app.config['OPENAPI_SPEC']['info']['version'],
@@ -62,43 +53,16 @@ def setup_app(app):
         **app.config['OPENAPI_SPEC']
     )
 
-    from flask_restful import Resource
-    # print(app.view_functions)
-    # for endpoint, func in app.view_functions:
-    #     print(func)
-    # spec.path(path=resource[1][0], view=resource[0].as_view())
-
-    # print(api.resources)
-    base_path = app.config['APPLICATION_CONTEXT']
-    with app.app_context():
-        for resource, urls, kwargs in api.resources:
-            for url in urls:
-                endpoint = kwargs.pop('endpoint')
-                # print(url.replace(base_path, ''))
-                view = next(v for v in app.view_functions.values() if v.__name__ == endpoint)
-                spec.path(path=url.replace(base_path, ''), view=view)
-
-    # # link specs to app
-    # docs.init_app(app)
-    #
-    # # resource discovery
-    # docs.register_existing_resources()
-
-    from pprint import pprint
-    # pprint(list(app.view_functions.items()))
-
-    # print(api.resources)
-    print(spec.to_yaml())
-
-    # print(app.view_functions)
-    # spec.path(api)
-    # print(docs.spec.to_yaml())
-
-    # register schemas
-    # spec.components.schema('HealthCheck', schema=HealthCheckSchema)
+    # resource discovery
+    for view in app.view_functions.values():
+        spec.path(
+            view=view,
+            app=app,
+            basePath=app.config['APPLICATION_CONTEXT']
+        )
 
     # generate swagger from spec
-    swg = flasgger.Swagger(
+    flasgger.Swagger(
         app=app,
         config=app.config['SWAGGER'],
         template=flasgger.apispec_to_template(
@@ -108,17 +72,8 @@ def setup_app(app):
         merge=True
     )
 
-    with app.app_context():
-        import yaml
-        print(yaml.dump(swg.get_apispecs('swagger')))
-
-    # print(app.url_map)
-
-    # print('---')
-    # with app.app_context():
-    #     import pprint
-    #     import yaml
-    #     print(yaml.dump(swg.get_apispecs('swagger')))
+    # redirect root path to context root
+    app.add_url_rule('/', 'index', view_func=lambda: redirect(url_for('flasgger.apidocs')))
 
     # register cli commands
     app.cli.add_command(test_command)
