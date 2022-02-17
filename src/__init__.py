e459f84c30df7526c342dda92c814cd4ed8bfe9d
Renato Teixeira <rena2damas@gmail.com>
2022-02-17 11:19:55 +0000

    PEP fix

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ try:
     from importlib import metadata
 except ImportError:
     import importlib_metadata as metadata  # python<=3.7
+
 import src.resources.health_checks
 
 __version__ = metadata.version("monitoring-service")
