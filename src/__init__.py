a416850a47893c15df37d3f56398a93a8ebf630f
Renato Teixeira <rena2damas@gmail.com>
2022-02-17 16:21:38 +0000

    refactor: define version and meta information #wip

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,5 @@ try:
 except ImportError:
     import importlib_metadata as metadata  # python<=3.7
 
-import src.resources.health_checks
-
-__version__ = metadata.version("monitoring-service")
+__meta__ = metadata.metadata("monitoring-service")
+__version__ = __meta__['version']
