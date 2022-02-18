f68d83b631aa1bd524ecb6fb40f6b51945a3b0b3
Renato Teixeira <rena2damas@gmail.com>
2022-02-18 19:10:40 +0000

    Use url_prefix variable

--- tests/functional/test_app.py
+++ tests/functional/test_app.py
@@ -4,18 +4,18 @@ class TestApp:
         """Ensure app is created."""
         assert app is not None
 
-    def test_redirect_root_to_application_root(self, client, ctx):
+    def test_redirect_root_to_application_root(self, client, url_prefix):
         """Ensure root path is redirected to application root."""
-        response = client.get('/')
+        response = client.get("/")
         assert response.status_code == 302
 
         response = client.get('/', follow_redirects=True)
         request = response.request
         assert response.status_code == 200
-        assert request.path.rstrip('/') == ctx
+        assert request.path.rstrip('/') == url_prefix
 
-    def test_swagger_apidocs(self, client, ctx):
+    def test_swagger_apidocs(self, client, url_prefix):
         """Ensure app serves swagger specs."""
-        response = client.get(f"{ctx}/swagger.json")
+        response = client.get(f"{url_prefix}/swagger.json")
 
         assert response.status_code == 200
