683c15364cb99cd39dcf952822b86da1ccee325c
Renato Teixeira <rena2damas@gmail.com>
2022-02-18 15:55:09 +0000

    test: run tests against pytest #wip

--- tests/functional/test_app.py
+++ tests/functional/test_app.py
@@ -1,13 +1,11 @@
-import pytest
-
-
-
 class TestApp:
 
     def test_can_create_app(self, app):
+        """Ensure app is created."""
         assert app is not None
 
-    def test_redirect_root_to_application_context(self, client, ctx):
+    def test_redirect_root_to_application_root(self, client, ctx):
+        """Ensure root path is redirected to application root."""
         response = client.get('/')
         assert response.status_code == 302
 
@@ -17,6 +15,7 @@ class TestApp:
         assert request.path.rstrip('/') == ctx
 
     def test_swagger_apidocs(self, client, ctx):
+        """Ensure app serves swagger specs."""
         response = client.get(f"{ctx}/swagger.json")
 
         assert response.status_code == 200
--- tests/functional/test_health_checks.py
+++ tests/functional/test_health_checks.py
@@ -1,55 +1,45 @@
+import pytest
 import json
-import unittest
-from unittest.mock import patch
 
-from src.app import create_app
+from src.api.bright import BrightAPI
 
 
-class TestHealthCheckAPI(unittest.TestCase):
+@pytest.fixture(scope="function", params=(7, 8))
+def bright(request):
+    return BrightAPI(
+        host='localhost',
+        port=80,
+        protocol='http',
+        version=request.param
+    )
 
-    def setUp(self):
-        self.app = create_app(config_name='testing')
-        self.client = self.app.test_client()
-        self.ctx = self.app.config['APPLICATION_CONTEXT']
 
-        # self.bright = Bright(version='latest')
-        # bright.health_checks()
+class TestHealthCheckAPI:
 
-    def tearDown(self):
-        pass
+    def test_get_no_health_checks(self, ctx, client, app):
+        """Ensure GET request retrieves health checks."""
+        response = client.get(f"{ctx}/health-checks")
+        print(app.config)
+        print(11111111111111111)
+        print(response.json)
+        # content = json.loads(response.json)
+        #
+        assert False
+        # assert response.status_code == 200
+        # assert content == []
 
-    # def test_get_health_checks(self):
-    #     """GET request to retrieve a list of health checks."""
-    #     response = self.client.get('/health-checks')
+    # def test_get_some_health_checks(self, client, ctx):
+    #     """Ensure GET request retrieves health checks."""
+    #     response = client.get(f"{ctx}/health-checks")
+    #     content = json.loads(response.json)
     #
-    #     self.assertEqual(response.status_code, 200)
-
-    @patch('bright')
-    def test_get_no_health_checks(self, mock_bright):
-        """GET request to retrieve health checks."""
-        mock_bright.health_checks.return_value = []
-
-        response = self.client.get(f"{self.ctx}/health-checks")
-        content = json.loads(response.json)
-
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(content, [])
-
-    @patch('bright')
-    def test_get_some_health_checks(self, mock_bright):
-        """GET request to retrieve health checks."""
-        mock_bright.health_checks.return_value = [1, 2, 3]
-
-        response = self.client.get(f"{self.ctx}/health-checks")
-        content = json.loads(response.json)
-
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(content, [1, 2, 3])
-
-    def test_get_health_check_by_id(self):
-        """GET request to retrieve an health check by id."""
-        pass
-
-    def test_get_missing_health_check(self):
-        """GET request that cannot retrieve an health check by id."""
-        pass
+    #     assert response.status_code == 200
+    #     assert content == [1, 2, 3]
+    #
+    # def test_get_health_check_by_id(self):
+    #     """Ensure GET request retrieves an health check by id."""
+    #     pass
+    #
+    # def test_get_missing_health_check(self):
+    #     """Ensure GET request cannot retrieve an health check by id."""
+    #     pass
