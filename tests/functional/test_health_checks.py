11705244fbd21d1188d5918f77a4ff88034df25e
Renato Teixeira <rena2damas@gmail.com>
2022-02-19 10:02:38 +0000

    Run test with mock

--- tests/functional/test_health_checks.py
+++ tests/functional/test_health_checks.py
@@ -1,7 +1,8 @@
 import pytest
-import json
+from unittest.mock import MagicMock, Mock
 
 from src.api.bright import BrightAPI
+from src.resources import health_checks
 
 
 @pytest.fixture(scope="function", params=(7, 8))
@@ -10,20 +11,21 @@ def bright(request):
         host='localhost',
         port=80,
         protocol='http',
+        basic_auth=('user', 'pass'),
         version=request.param
     )
 
 
 class TestHealthCheckAPI:
 
-    def test_get_no_health_checks(self, url_prefix, client):
+    def test_get_no_health_checks(self, url_prefix, client, bright, mocker):
         """Ensure GET request retrieves health checks."""
+        mock = Mock(return_value=bright)
+        mock.health_checks.return_value = []
+        mocker.patch.object(health_checks, 'BrightAPI', return_value=mock)
         response = client.get(f"{url_prefix}/health-checks")
-        content = json.loads(response.json)
-
-        assert False
-        # assert response.status_code == 200
-        # assert content == []
+        assert response.status_code == 200
+        assert response.json == []
 
     # def test_get_some_health_checks(self, client, ctx):
     #     """Ensure GET request retrieves health checks."""
