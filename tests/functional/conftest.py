142e55f1bcbb6e909dec0518e8c3e4629b28b0ea
Renato Teixeira <rena2damas@gmail.com>
2022-02-21 10:04:26 +0000

    Create app with given configs

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -3,23 +3,18 @@ import pytest
 from src.app import create_app
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='module')
 def app():
-    app = create_app(config_name='testing')
-    app.config.update({
+    app = create_app(config_name='testing', configs={
         'FLASK_RUN_HOST': 'localhost',
-        'FLASK_RUN_PORT': 5000,
-        'SUPPORTED_MEASURABLES': ['foo']
+        'FLASK_RUN_PORT': str(5000),
+        'APPLICATION_ROOT': '/',
+        'SUPPORTED_MEASURABLES': 'foo'
     })
     with app.test_request_context():
         yield app
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='module')
 def client(app):
     return app.test_client()
-
-
-@pytest.fixture(scope='session')
-def url_prefix(app):
-    return app.config['APPLICATION_ROOT']
