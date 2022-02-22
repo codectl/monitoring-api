8ecdaf288ee55a631ccbd95dd77b0e7dc11994b8
Renato Teixeira <rena2damas@gmail.com>
2022-02-22 20:05:14 +0000

    Make endpoint route health-check compliant with Flask routing

--- src/resources/health_checks.py
+++ src/resources/health_checks.py
@@ -30,7 +30,7 @@ class HealthChecks(Resource):
         return HealthCheckSchema(many=True).dump(health_checks)
 
 
-@api.resource('/health-check/{key}', endpoint='health-check')
+@api.resource('/health-checks/<key>', endpoint='health-check')
 class HealthCheck(Resource):
 
     def get(self, key):
