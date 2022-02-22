4a4d9618cd059971df057492734765035e4b60ce
Renato Teixeira <rena2damas@gmail.com>
2022-02-22 10:28:03 +0000

    Fixture correction

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,20 +1,23 @@
 import pytest
 
 from src.app import create_app
+from src.settings.oas import swagger_configs
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='class')
 def app():
-    app = create_app(config_name='testing', env={
+    app = create_app(config_name='testing', dotenv=False, configs={
         'FLASK_RUN_HOST': 'localhost',
         'FLASK_RUN_PORT': str(5000),
         'APPLICATION_ROOT': '/',
-        'SUPPORTED_MEASURABLES': 'foo'
+        'SUPPORTED_MEASURABLES': 'foo',
+        'OPENAPI': '3.0.3',
+        'SWAGGER': swagger_configs(openapi_version='3.0.3', app_root='/')
     })
     with app.test_request_context():
         yield app
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='class')
 def client(app):
     return app.test_client()
