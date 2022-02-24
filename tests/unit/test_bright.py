d39d25ae02a50c0941ffddec470e5077b1613ff6
Renato Teixeira <rena2damas@gmail.com>
2022-02-24 17:28:41 +0000

    Test get health check

--- tests/unit/test_bright.py
+++ tests/unit/test_bright.py
@@ -3,6 +3,8 @@ import re
 import pytest
 
 from src.api.bright import BrightAPI
+from src.models.bright import HealthCheck, HealthCheckStatus
+from src.schemas.serlializers.bright import HealthCheckSchema
 
 
 @pytest.fixture(scope="class", params=(7, 8))
@@ -45,7 +47,7 @@ class TestBrightAPI:
         )
         assert bright._session.cert == ('cert', 'file')
 
-    @pytest.mark.parametrize('version', (7, 8))
+    @pytest.mark.parametrize('version', (7, 8, 7.2, 8.2, '7.2', '8.2'))
     def test_bright_default_version(self, version, requests_mock):
         matcher = re.compile(r"localhost.*")
         requests_mock.register_uri('POST', matcher, json={'cmVersion': version})
@@ -53,4 +55,65 @@ class TestBrightAPI:
             host='localhost',
             cert_auth=('cert', 'file')
         )
-        # assert bright.version ==
+        major_version = int(float(version))
+        assert bright.version == version
+        assert type(bright.instance).__name__ == f"Bright{major_version}"
+
+    @pytest.mark.parametrize('value', ('PASS', 'FAIL', 'UNKNOWN'))
+    def test_bright8_health_check(self, value, mocker, requests_mock):
+        bright = BrightAPI(
+            host='localhost',
+            basic_auth=('user', 'pass'),
+            version=8
+        )
+        matcher = re.compile(rf"{bright.url}.*")
+        mocker.patch.object(bright, 'supported_measurables', return_value=['foo'])
+        data = {'data': [{
+            'age': 0,
+            'entity': 'foo_node',
+            'measurable': 'foo',
+            'time': 0,
+            'value': value,
+            'raw': {}
+        }]}
+        requests_mock.register_uri('GET', matcher, json=data)
+        expected = HealthCheck(
+            name='foo',
+            status=HealthCheckStatus(value),
+            node='foo_node',
+            timestamp=0,
+            seconds_ago=0,
+            raw=data['data'][0]
+        )
+
+        health_check = bright.health_check('foo')
+        assert health_check == expected
+
+    # def test_bright7_health_check(self, mocker, requests_mock):
+    #     bright = BrightAPI(
+    #         host='localhost',
+    #         basic_auth=('user', 'pass'),
+    #         version=7
+    #     )
+    #     matcher = re.compile(rf"{bright.url}.*")
+    #     mocker.patch.object(bright, 'supported_measurables', return_value=['foo'])
+    #     data = {'data': [{
+    #         'age': 0,
+    #         'entity': 'foo_node',
+    #         'measurable': 'foo',
+    #         'time': 0,
+    #         'value': 'PASS',
+    #         'raw': {}
+    #     }]}
+    #     requests_mock.register_uri('GET', matcher, json=data)
+    #     expected = HealthCheck(
+    #         name='foo',
+    #         status=HealthCheckStatus.ONLINE,
+    #         node='foo_node',
+    #         timestamp=0,
+    #         seconds_ago=0,
+    #         raw=data['data'][0]
+    #     )
+    #
+    #     health_check = bright.health_check('foo')
+    #     assert health_check == expected
