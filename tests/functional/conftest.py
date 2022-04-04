90c94bcb52d09ab90f60e687f5a34b7e952e3b24
Renato Teixeira <rena2damas@gmail.com>
2022-04-04 11:33:20 +0100

    Change config variable placement

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -12,8 +12,8 @@ def app():
             "FLASK_RUN_HOST": "localhost",
             "FLASK_RUN_PORT": 5000,
             "APPLICATION_ROOT": "/",
-            "SUPPORTED_MEASURABLES": ["foo", "bar"],
             "OPENAPI": "3.0.3",  # default version
+            "SUPPORTED_MEASURABLES": ["foo", "bar"],
         },
     )
     with app.test_request_context():
